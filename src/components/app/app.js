d5b5447ec99af1620609b29ffc45018e13f6d7d8
anton_kuzmitsky <kuzmitsky@a2design.biz>
2020-02-06 17:04:30 +0600

    send first prop

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Main from '../main/main';
+
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {rentAmount} = props;
+
+  return (
+    <Main
+      rentAmount={ rentAmount }
+    />
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
 
+const Options = {
+  RENT_AMOUNT: 312,
+};
+
+ReactDOM.render(
+    <App
+      rentAmount={ Options.RENT_AMOUNT }
+    />,
+    document.getElementById(`root`)
+);
