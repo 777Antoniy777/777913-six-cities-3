998fdd29ba64f5bac8a01df591d52ead38e79169
Anton_Kuzmitsky <tat7106@gmail.com>
2020-02-07 21:51:27 +0600

    add propTypes and add cycle for cards

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import Main from '../main/main';
 
-const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {rentAmount} = props;
-
+const App = ({rentAmount, apartments}) => {
   return (
     <Main
       rentAmount={ rentAmount }
+      apartments={ apartments }
     />
   );
+};
+
+App.propTypes = {
+  rentAmount: PropTypes.number.isRequired,
+  apartments: PropTypes.arrayof(
+      PropTypes.number,
+      PropTypes.string
+  ).isRequired,
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,9 +6,48 @@ const Options = {
   RENT_AMOUNT: 312,
 };
 
+const apartments = [
+  {
+    id: 1,
+    title: `Beautiful & luxurious apartment at great location`,
+    src: `img/apartment-01.jpg`,
+    price: 120,
+    type: `Apartment`,
+  },
+  {
+    id: 2,
+    title: `Wood and stone place`,
+    src: `img/room.jpg`,
+    price: 80,
+    type: `Private room`,
+  },
+  {
+    id: 3,
+    title: `Canal View Prinsengracht`,
+    src: `img/apartment-02.jpg`,
+    price: 132,
+    type: `Apartment`,
+  },
+  {
+    id: 4,
+    title: `Nice, cozy, warm big bed apartment`,
+    src: `img/apartment-03.jpg`,
+    price: 180,
+    type: `Apartment`,
+  },
+  {
+    id: 5,
+    title: `Wood and stone place`,
+    src: `img/room.jpg`,
+    price: 80,
+    type: `Private room`,
+  },
+];
+
 ReactDOM.render(
     <App
       rentAmount={ Options.RENT_AMOUNT }
+      apartments={ apartments }
     />,
     document.getElementById(`root`)
 );
