6982582c7fa559abfc09f1525c8175f6ce13ed7c
Anton_Kuzmitsky <tat7106@gmail.com>
2020-04-09 10:29:45 +0600

    type private-route

--- src/components/private-route/private-route.tsx
+++ src/components/private-route/private-route.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {Route, Redirect} from "react-router-dom";
 
 type Props = {
-  component: () => Element,
+  component: () => React.ReactElement,
   authorizationStatus: string,
   condRedirect: string,
   linkRedirect: string,
